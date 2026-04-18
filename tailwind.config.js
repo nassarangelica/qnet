e9a609d1435b97a7a5cfc5c6b7d6ec58b24bfb70
nassarangelica <angelicanassar039@gmail.com>
2026-04-18 10:35:11 +0800

    fix: add useUnreadMessages hook and fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.ts
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -16,5 +13,3 @@ const config: Config = {
   },
   plugins: [],
 };
-
-export default config;
